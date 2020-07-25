3d5be14df31ca5807da1e315d080ffb417108241
E01174 <E01174@GGNS1LTW93B.hsc.com>
2020-07-25 13:15:49 +0530

    Updated inventory service

--- Training/Microservices/Ordering/ms-inventory-service/src/main/java/com/hughes/controllers/InventoryController.java
+++ Training/Microservices/Ordering/ms-inventory-service/src/main/java/com/hughes/controllers/InventoryController.java
@@ -1,15 +1,15 @@
 package com.hughes.controllers;
 
-
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.PathVariable;
-
 
 import com.hughes.entities.Inventory;
 import com.hughes.services.InventoryService;
@@ -17,18 +17,28 @@ import com.hughes.services.InventoryService;
 @RestController
 @RequestMapping("/inventory")
 public class InventoryController {
-	
+
 	@Autowired
 	InventoryService invService;
-	
-	@GetMapping("/get/item/{name}")
-	public Inventory getItemDetails(@PathVariable String name) {
-		return invService.getItemDetails(name);
+
+	@GetMapping("/get/{name}")
+	public Inventory getItem(@PathVariable String name) {
+		return invService.getItem(name);
 	}
-	
+
+	@GetMapping("/get/all")
+	public List<Inventory> getAllItems() {
+		return invService.getAllItems();
+	}
+
 	@PostMapping("/add")
-	public Inventory addInventory(@RequestBody Inventory inventory) {		
-		return invService.add(inventory);
+	public Inventory addItem(@RequestBody Inventory inventory) {
+		return invService.addItem(inventory);
+	}
+
+	@PutMapping("/update")
+	public Inventory updateItem(@RequestBody Inventory inventory) {
+		return invService.updateItem(inventory);
 	}
 
 }
--- Training/Microservices/Ordering/ms-inventory-service/src/main/java/com/hughes/services/InventoryService.java
+++ Training/Microservices/Ordering/ms-inventory-service/src/main/java/com/hughes/services/InventoryService.java
@@ -1,5 +1,7 @@
 package com.hughes.services;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -8,17 +10,31 @@ import com.hughes.repos.InventoryRepository;
 
 @Service
 public class InventoryService {
-	
+
 	@Autowired
 	private InventoryRepository inventoryRepo;
-	
-	public Inventory add(Inventory inventory) {
+
+	public Inventory addItem(Inventory inventory) {
 		return inventoryRepo.save(inventory);
 	}
 
-	public Inventory getItemDetails(String item) {
+	public Inventory updateItem(Inventory updatedInventory) {
+		Inventory inventory = getItem(updatedInventory.getItem());
+
+		inventory.setQuantity(updatedInventory.getQuantity());
+		inventory.setPrice(updatedInventory.getPrice());
+
+		return inventoryRepo.save(inventory);
+	}
+
+	public Inventory getItem(String item) {
 		System.out.println("Received Query for item:" + item);
+
 		return inventoryRepo.findByItem(item);
+	}
+
+	public List<Inventory> getAllItems() {
+		return inventoryRepo.findAll();
 	}
 
 }
