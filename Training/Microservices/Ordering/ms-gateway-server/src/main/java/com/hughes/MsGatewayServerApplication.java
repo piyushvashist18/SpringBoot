f42adb81deae8a25b794df5846bf5487afc5ccc5
piyushvashist18 <piyush.vashist@hsc.com>
2020-07-26 01:08:27 +0530

    Added Api gateway

---
+++ Training/Microservices/Ordering/ms-gateway-server/src/main/java/com/hughes/MsGatewayServerApplication.java
@@ -0,0 +1,17 @@
+package com.hughes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+
+@EnableZuulProxy
+@EnableDiscoveryClient
+@SpringBootApplication
+public class MsGatewayServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsGatewayServerApplication.class, args);
+	}
+
+}
